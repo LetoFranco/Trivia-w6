400d9e84aebce88edaf8ad787770d8cb946614b6
Franco Leto <letoefranco@gmail.com>
2022-09-07 22:52:58 -0300

    Class 2 - Auto Layout - Interface Builder - Adding Constraints - Adding Constraints by code - Wiring IBOutlet and IBAction - Finishing UI for Initial View Controller

--- Trivia/Features/StartGame/InitialViewController.swift
+++ Trivia/Features/StartGame/InitialViewController.swift
@@ -9,8 +9,31 @@ import UIKit
 
 class InitialViewController: UIViewController {
   
+  // MARK: - Attributes
+  
+  @IBOutlet weak var logoImageView: UIImageView!
+  @IBOutlet weak var userTextField: UITextField!
+  
+  // MARK: - View Controller's Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+   
+    addLogoConstraint()
+  }
+  
+  // MARK: - Actions
+  
+  @IBAction func startTriviaTapped(_ sender: Any) {
+    let name = userTextField.text!
+    print("Botón Empezar tocado. El nombre del usuario es: \(name)")
+  }
+  
+  private func addLogoConstraint() {
+    logoImageView.translatesAutoresizingMaskIntoConstraints = false
+    logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20).isActive = true
+    logoImageView.widthAnchor.constraint(equalToConstant: 150).isActive = true
+    logoImageView.heightAnchor.constraint(equalToConstant: 150).isActive = true
+    let centerHorizontalConstraint = logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+    NSLayoutConstraint.activate([centerHorizontalConstraint])
   }
 }
