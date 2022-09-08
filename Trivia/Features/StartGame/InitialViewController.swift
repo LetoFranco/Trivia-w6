df9ac42a359ccf8502a36f2322aae7777bd547ba
Franco Leto <letoefranco@gmail.com>
2022-09-07 22:26:57 -0300

    Class 1 - Removing Main.Storyboard - Creating Initial View Controller - Setting Initial View Controller as First View Controller in SceneDelegate - Placing Image View with frames in Initial View Controller - Modifying Info.plist to stop supporting Main.Storyboard as first screen

---
+++ Trivia/Features/StartGame/InitialViewController.swift
@@ -0,0 +1,16 @@
+//
+//  InitialViewController.swift
+//  Trivia
+//
+//  Created by Franco Leto on 07/09/2022.
+//
+
+import UIKit
+
+class InitialViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+}
